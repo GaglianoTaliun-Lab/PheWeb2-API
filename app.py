028ee90ca2fd20730e36c5826018513d15e60cf6
Hongyu Xiao <xiaohongyu12138@gmail.com>
2024-10-15 14:11:54 -0400

    enable port selection when running app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os import getenv
+import os 
 from flask import Flask
 from blueprints import phenotype_routes
 from flask_cors import CORS
@@ -11,4 +11,5 @@ CORS(app, origins=app.config['CORS_ORIGINS'])
 app.register_blueprint(phenotype_routes.bp)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9099)
+    port = int(os.environ.get('PORT', 9099))
+    app.run(host='127.0.0.1', port=port)
