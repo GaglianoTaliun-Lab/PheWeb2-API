5dfb5647fe5bce09c6f6c6086a7bcb1203d8adda
JustinBellavance <justin.bellavance@gmail.com>
2025-06-04 14:09:58 -0400

    added config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env")
 
+CORS_ORIGINS = os.getenv("CORS_ORIGINS", "").split(",")
+
 BASE_DIR = os.getenv("BASE_DIR")
 
 PHENOTYPES_DIR = os.path.join(BASE_DIR)
@@ -20,8 +22,12 @@ MANHATTAN_PEAK_PVAL_THRESHOLD = 1e-6
 MANHATTAN_PEAK_SPRAWL_DIST = 200_000
 MANHATTAN_PEAK_VARIANT_COUNTING_PVAL_THRESHOLD = 5e-8
 
-# CORS_ORIGINS = ['http://localhost:8099']
-CORS_ORIGINS = os.getenv("CORS_ORIGINS", "").split(",")
+# Filtering parameters
+MIN_IMP_QUALITY = 0.3
+INTERACTION_MAC_THRESHOLD = 160
+
+# Path to the file containing the R2/Imputation Quality values (optional)
+R2_FILE = os.getenv("R2_DIR")
 
 hg_build_number = 38
 
@@ -35,10 +41,16 @@ field_aliases = {
     "ALLELE0": "ref",
     "ALLELE1": "alt",
     "A1FREQ": "af",
+    # Add this field if you have imputation quality scores in the GWAS results
+    # "INFO": "imp_quality",
+    # Start your custom field (e.g. imp_quality) with "file://" to load from a file (accept pvar or vcf files)
+    # "file://path/file.pvar,R2": "imp_quality",
+    f"file://{R2_FILE},R2": "imp_quality",
+    "N": "n_samples",
     "BETA": "beta",
     "SE": "sebeta",
     "LOG10P": "pval",
     "TEST": "test",
 }
 
-interaction_aliases = {"BSEX": "sex"}
+interaction_aliases = {"sex": "sex"}
